7cb4562997698bc6a753ef942bed9212822f5181
breezesway <1048662534@qq.com>
2023-04-26 15:06:00 +0800

    实现会员注册接口

--- member/src/main/java/com/cgz/ticketing/member/service/MemberService.java
+++ member/src/main/java/com/cgz/ticketing/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.cgz.ticketing.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.cgz.ticketing.member.domain.Member;
+import com.cgz.ticketing.member.domain.MemberExample;
 import com.cgz.ticketing.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -12,5 +17,20 @@ public class MemberService {
 
     public int count(){
         return (int) memberMapper.countByExample(null);
+    }
+
+    public long register(String mobile){
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+        if(CollUtil.isNotEmpty(list)){
+            //return list.get(0).getId();
+            throw new RuntimeException("手机号已注册");
+        }
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
