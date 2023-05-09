6003c37118b7c8c7730cfc9d734ca53368d3fee2
breezesway <1048662534@qq.com>
2023-05-09 14:50:33 +0800

    增加路由登录拦截，访问所有控台页面都需要登录

--- web/src/views/main.vue
+++ web/src/views/main.vue
@@ -22,8 +22,8 @@
 import {defineComponent, ref} from 'vue';
 import TheHeaderView from "@/components/the-header.vue";
 import TheSiderView from "@/components/the-sider.vue";
-import axios from "axios";
-import {notification} from "ant-design-vue";
+//import axios from "axios";
+//import {notification} from "ant-design-vue";
 //import store from "@/store";
 export default defineComponent({
     components: {
@@ -32,14 +32,14 @@ export default defineComponent({
     },
     setup() {
         const count = ref(0);
-        axios.get("/member/member/count").then((response) => {
-            let data = response.data;
-            if (data.success) {
-                count.value = data.content;
-            } else {
-                notification.error({ description: data.message });
-            }
-        });
+        // axios.get("/member/member/count").then((response) => {
+        //     let data = response.data;
+        //     if (data.success) {
+        //         count.value = data.content;
+        //     } else {
+        //         notification.error({ description: data.message });
+        //     }
+        // });
         return {
             count
         };
