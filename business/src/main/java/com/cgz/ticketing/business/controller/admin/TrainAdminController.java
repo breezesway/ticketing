7c3dac7acb0c2d6a2d2565456d9351eeddfd0bfc
breezesway <1048662534@qq.com>
2023-05-21 22:49:38 +0800

    增加火车车次下拉框选择功能

--- business/src/main/java/com/cgz/ticketing/business/controller/admin/TrainAdminController.java
+++ business/src/main/java/com/cgz/ticketing/business/controller/admin/TrainAdminController.java
@@ -1,15 +1,16 @@
 package com.cgz.ticketing.business.controller.admin;
 
-import com.cgz.ticketing.common.context.LoginMemberContext;
-import com.cgz.ticketing.common.resp.CommonResp;
-import com.cgz.ticketing.common.resp.PageResp;
 import com.cgz.ticketing.business.req.TrainQueryReq;
 import com.cgz.ticketing.business.req.TrainSaveReq;
 import com.cgz.ticketing.business.resp.TrainQueryResp;
 import com.cgz.ticketing.business.service.TrainService;
+import com.cgz.ticketing.common.resp.CommonResp;
+import com.cgz.ticketing.common.resp.PageResp;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/train")
@@ -34,6 +35,12 @@ public class TrainAdminController {
     public CommonResp<Object> delete(@PathVariable Long id) {
         trainService.delete(id);
         return new CommonResp<>();
+    }
+
+    @GetMapping("/query-all")
+    public CommonResp<List<TrainQueryResp>> queryList() {
+        List<TrainQueryResp> list = trainService.queryAll();
+        return new CommonResp<>(list);
     }
 
 }
