e31ec2602ef67ee93733bdc791e99b0be7261e88
breezesway <1048662534@qq.com>
2023-05-31 22:03:37 +0800

    增加quartz的12张表，并增加接口来操作quartz

--- batch/src/main/java/com/cgz/ticketing/batch/job/TestJob.java
+++ batch/src/main/java/com/cgz/ticketing/batch/job/TestJob.java
@@ -1,14 +1,15 @@
 package com.cgz.ticketing.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution
 public class TestJob implements Job {
 
     @Override
-    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob TEST");
-
+    public void execute(JobExecutionContext jobExecutionContext) {
+        System.out.println("TestJob TEST 开始");
+        System.out.println("TestJob TEST 结束");
     }
 }
---
+++ batch/src/main/java/com/cgz/ticketing/batch/req/CronJobReq.java
@@ -0,0 +1,14 @@
+package com.cgz.ticketing.batch.req;
+
+import lombok.Data;
+
+@Data
+public class CronJobReq {
+    private String group;
+
+    private String name;
+
+    private String description;
+
+    private String cronExpression;
+}
