8fc28970434ff6025eadca61c1ff43ba267ff802
breezesway <1048662534@qq.com>
2023-06-10 19:50:55 +0800

    给每日车站页面增加日期和车次查询

--- business/src/main/java/com/cgz/ticketing/business/req/DailyTrainStationQueryReq.java
+++ business/src/main/java/com/cgz/ticketing/business/req/DailyTrainStationQueryReq.java
@@ -3,9 +3,22 @@ package com.cgz.ticketing.business.req;
 import com.cgz.ticketing.common.req.PageReq;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class DailyTrainStationQueryReq extends PageReq {
 
+    /**
+     * 日期
+     */
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    /**
+     * 车次编号
+     */
+    private String trainCode;
 }
