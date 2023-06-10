d5afbb7abfd35789ecc4621ebae4f4ea21f0d9b0
breezesway <1048662534@qq.com>
2023-06-10 19:48:38 +0800

    生成每日车站数据管理功能

---
+++ business/src/main/java/com/cgz/ticketing/business/controller/admin/DailyTrainStationAdminController.java
@@ -0,0 +1,39 @@
+package com.cgz.ticketing.business.controller.admin;
+
+import com.cgz.ticketing.common.context.LoginMemberContext;
+import com.cgz.ticketing.common.resp.CommonResp;
+import com.cgz.ticketing.common.resp.PageResp;
+import com.cgz.ticketing.business.req.DailyTrainStationQueryReq;
+import com.cgz.ticketing.business.req.DailyTrainStationSaveReq;
+import com.cgz.ticketing.business.resp.DailyTrainStationQueryResp;
+import com.cgz.ticketing.business.service.DailyTrainStationService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/daily-train-station")
+public class DailyTrainStationAdminController {
+
+    @Resource
+    private DailyTrainStationService dailyTrainStationService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody DailyTrainStationSaveReq req) {
+        dailyTrainStationService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<DailyTrainStationQueryResp>> queryList(@Valid DailyTrainStationQueryReq req) {
+        PageResp<DailyTrainStationQueryResp> list = dailyTrainStationService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        dailyTrainStationService.delete(id);
+        return new CommonResp<>();
+    }
+
+}
---
+++ business/src/main/java/com/cgz/ticketing/business/req/DailyTrainStationQueryReq.java
@@ -0,0 +1,11 @@
+package com.cgz.ticketing.business.req;
+
+import com.cgz.ticketing.common.req.PageReq;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class DailyTrainStationQueryReq extends PageReq {
+
+}
