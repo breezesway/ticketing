3023ba76563adf59d9f85ce58da49fabb18f8451
breezesway <1048662534@qq.com>
2023-06-22 17:48:17 +0800

    使用openfeign实现服务间的调用

--- batch/src/main/java/com/cgz/ticketing/batch/config/BatchApplication.java
+++ batch/src/main/java/com/cgz/ticketing/batch/config/BatchApplication.java
@@ -5,12 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 @ComponentScan("com.cgz")
 @MapperScan("com.cgz.ticketing.*.mapper")
+@EnableFeignClients("com.cgz.ticketing.batch.feign")
 public class BatchApplication {
 
     private static final Logger LOG  = LoggerFactory.getLogger(BatchApplication.class);
