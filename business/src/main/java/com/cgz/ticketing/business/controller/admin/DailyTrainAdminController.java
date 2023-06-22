a1e4076f4b9946267d47c3e4e2b3227a4ed4b44f
breezesway <1048662534@qq.com>
2023-06-22 19:03:59 +0800

    business模块新增生成某日全部车次数据功能

--- business/src/main/java/com/cgz/ticketing/business/controller/admin/DailyTrainAdminController.java
+++ business/src/main/java/com/cgz/ticketing/business/controller/admin/DailyTrainAdminController.java
@@ -1,15 +1,17 @@
 package com.cgz.ticketing.business.controller.admin;
 
-import com.cgz.ticketing.common.context.LoginMemberContext;
-import com.cgz.ticketing.common.resp.CommonResp;
-import com.cgz.ticketing.common.resp.PageResp;
 import com.cgz.ticketing.business.req.DailyTrainQueryReq;
 import com.cgz.ticketing.business.req.DailyTrainSaveReq;
 import com.cgz.ticketing.business.resp.DailyTrainQueryResp;
 import com.cgz.ticketing.business.service.DailyTrainService;
+import com.cgz.ticketing.common.resp.CommonResp;
+import com.cgz.ticketing.common.resp.PageResp;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/admin/daily-train")
@@ -33,6 +35,18 @@ public class DailyTrainAdminController {
     @DeleteMapping("/delete/{id}")
     public CommonResp<Object> delete(@PathVariable Long id) {
         dailyTrainService.delete(id);
+        return new CommonResp<>();
+    }
+
+    @DeleteMapping("/gen-daily/{date}")
+    public CommonResp<Object> delete(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
+        dailyTrainService.genDaily(date);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/gen-daily/{date}")
+    public CommonResp<Object> genDaily(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
+        dailyTrainService.genDaily(date);
         return new CommonResp<>();
     }
 
