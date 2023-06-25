ee97cc2b0801fc714c7aa6bfe761db510f378040
breezesway <1048662534@qq.com>
2023-06-25 23:43:52 +0800

    为余票信息表生成后端代码

---
+++ business/src/main/java/com/cgz/ticketing/business/controller/admin/DailyTrainTicketAdminController.java
@@ -0,0 +1,39 @@
+package com.cgz.ticketing.business.controller.admin;
+
+import com.cgz.ticketing.common.context.LoginMemberContext;
+import com.cgz.ticketing.common.resp.CommonResp;
+import com.cgz.ticketing.common.resp.PageResp;
+import com.cgz.ticketing.business.req.DailyTrainTicketQueryReq;
+import com.cgz.ticketing.business.req.DailyTrainTicketSaveReq;
+import com.cgz.ticketing.business.resp.DailyTrainTicketQueryResp;
+import com.cgz.ticketing.business.service.DailyTrainTicketService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/daily-train-ticket")
+public class DailyTrainTicketAdminController {
+
+    @Resource
+    private DailyTrainTicketService dailyTrainTicketService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody DailyTrainTicketSaveReq req) {
+        dailyTrainTicketService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<DailyTrainTicketQueryResp>> queryList(@Valid DailyTrainTicketQueryReq req) {
+        PageResp<DailyTrainTicketQueryResp> list = dailyTrainTicketService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        dailyTrainTicketService.delete(id);
+        return new CommonResp<>();
+    }
+
+}
