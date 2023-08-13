252ca1d6752ba6c58e6dbfe6698ca485bad02372
breezesway <1048662534@qq.com>
2023-08-13 15:45:22 +0800

    熔断处理

--- business/src/main/java/com/cgz/ticketing/business/config/BusinessApplication.java
+++ business/src/main/java/com/cgz/ticketing/business/config/BusinessApplication.java
@@ -30,8 +30,8 @@ public class BusinessApplication {
         LOG.info("地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
 
         // 限流规则
-        initFlowRules();
-        LOG.info("已定义限流规则");
+//        initFlowRules();
+//        LOG.info("已定义限流规则");
     }
 
     private static void initFlowRules(){
--- business/src/main/java/com/cgz/ticketing/business/controller/TestController.java
+++ business/src/main/java/com/cgz/ticketing/business/controller/TestController.java
@@ -1,13 +1,28 @@
 package com.cgz.ticketing.business.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.cgz.ticketing.business.service.TestService;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Resource
+    private TestService testService;
+
+    @SentinelResource("hello")
     @GetMapping("/hello")
-    public String hello(){
-        return "hello world! Business!";
+    public String hello() throws InterruptedException {
+        testService.hello2();
+        return "Hello World! Business!";
+    }
+
+    @SentinelResource("hello1")
+    @GetMapping("/hello1")
+    public String hello1() throws InterruptedException {
+        testService.hello2();
+        return "Hello World! Business1!";
     }
 }
