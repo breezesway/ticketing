83c4f84b6fbf594333df18e5c97726f242b1d1ad
breezesway <1048662534@qq.com>
2023-08-19 19:30:53 +0800

    修改MQ消息内容，只需通知出哪个车次的票，无需具体到人

---
+++ business/src/main/java/com/cgz/ticketing/business/dto/ConfirmOrderMQDto.java
@@ -0,0 +1,23 @@
+package com.cgz.ticketing.business.dto;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class ConfirmOrderMQDto {
+    /**
+     * 日志流程号，用于同转异时，用同一个流水号
+     */
+    private String logId;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * 车次编号
+     */
+    private String trainCode;
+}
